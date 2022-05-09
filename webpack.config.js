87bb570408e891c50f20a3768f77e4f1baf45969
poljik <poljik@gmail.com>
2022-05-09 09:57:33 +0300

    update webpack.config fo eslint check

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/scripts/index.js',
+  entry: "./src/scripts/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[hash].js',
-	  clean: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[hash].js",
+    clean: {
       keep: /\.git/,
     },
   },
   mode: "development",
   plugins: [
-	  new HtmlWebpackPlugin({
-		  title:'Virtual keyboard',
-		  filename:'index.html',
-		  template:'src/index.html',
-	  }),
+    new HtmlWebpackPlugin({
+      title:"Virtual keyboard",
+      filename:"index.html",
+      template:"src/index.html",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.s[ac]ss$/i,
